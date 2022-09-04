57916c921dd78847c84beb86a0c39050da3798a8
Eliton Alves <elitonmeunome_2014@hotmail.com>
2022-09-04 18:34:06 -0300

    adicionado logs ao git ignore

---
+++ RestorantiApplication/Generics/Logs/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestorantiApplication.Generics.Logs
+{
+    public static class Logger
+    {
+        public static string? logPath = ConfigurationManager.AppSettings["logPath"];
+        public static void Write(string Message)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(logPath))
+                {
+                    if (Directory.Exists(logPath.Substring(0, logPath.IndexOf("/"))))
+                    {
+                        if (!File.Exists(logPath))
+                            File.Create(logPath);
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(logPath.Substring(0, logPath.IndexOf("/")));
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(logPath, true))
+                    {
+                        writer.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - {Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
