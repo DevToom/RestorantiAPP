e1cc992378d9cf12b33991ae51e52b509aefb1a0
Eliton Alves <elitonmeunome_2014@hotmail.com>
2022-11-03 02:22:46 -0300

    Finalizado a tela de mesas, funcionalidades criada.

---
+++ RestorantiApplication/Models/Entities/Constants/TableConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestorantiApplication.Models.Entities.Constants
+{
+    public class TableConstants
+    {
+        /// <summary>
+        /// Status Inicial de uma mesa - quando o funcionário fechar a mesa no caixa.
+        /// </summary>
+        public const string OPEN_VALUE = "DISPONÍVEL";
+
+        /// <summary>
+        /// Status de uma mesa - quando uma conta for aberta na mesa
+        /// </summary>
+        public const string BLOCK_VALUE = "OCUPADA";
+    }
+}
--- RestorantiApplication/Views/Tables.cs
+++ RestorantiApplication/Views/Tables.cs
@@ -10,6 +10,7 @@ using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -192,17 +193,175 @@ namespace RestorantiApplication.Views
 
         private void BtnUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (BtnUpdate.Text.Equals("Editar"))
+                {
+                    var listOfAllSelected = ListTables.SelectedItems.Cast<ListViewItem>().ToList();
+                    if (listOfAllSelected?.Count > 0)
+                    {
+                        foreach (var i in listOfAllSelected)
+                        {
+                            //Id da mesa
+                            LblIdTable.Text = i.Text;
 
+                            //Número da mesa
+                            TxtTableNumber.Text = i.SubItems[1].Text;
+
+                            //Status da mesa
+                            ChkTableStatus.SelectedIndex = ChkTableStatus.Items.IndexOf(i.SubItems[2].Text);
+                            ChkTableStatus.Enabled = true;
+                        }
+
+                        BtnUpdate.Text = "Salvar";
+                    }
+                    else
+                    {
+                        ActionsGenerics.ShowMessage("Selecione uma mesa para conseguir editar!");
+                    }
+                }
+                else if (BtnUpdate.Text.Equals("Salvar"))
+                {
+                    _client = new HttpClient();
+
+                    var table = new Table
+                    {
+                        Id = Convert.ToInt32(LblIdTable.Text),
+                        TableNumber = Convert.ToInt32(TxtTableNumber.Text),
+                        TableStatus = ChkTableStatus.SelectedItem.ToString(),
+                        IsActive = true,
+                        ModifiedDate = DateTime.Now,
+                        ModifiedUserId = UserLogged.UserId
+                    };
+
+                    var request = JsonConvert.SerializeObject(table);
+                    var contentString = new StringContent(request, Encoding.UTF8, "application/json");
+                    contentString.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    HttpResponseMessage result = _client.PostAsync($"{_baseUrl}/Table/Update", contentString).Result;
+                    var response = JsonConvert.DeserializeObject<MessageResponse<Table>>(result.Content.ReadAsStringAsync().Result);
+
+                    if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ActionsGenerics.ShowMessage(response.Message);
+                        BtnUpdate.Text = "Editar";
+                        Tables_Load(sender, e);
+                    }
+
+                    if (result.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        //Caso exista alguma conta aberta para a mesa
+                        if (response.Message.Contains("Existe uma conta atualmente com o status de"))
+                        {
+                            if (ActionsGenerics.ConfirmCustom(response.Message))
+                            {
+                                result = _client.PostAsync($"{_baseUrl}/Table/ConfirmUpdate", contentString).Result;
+                                response = JsonConvert.DeserializeObject<MessageResponse<Table>>(result.Content.ReadAsStringAsync().Result);
+                                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                                {
+                                    ActionsGenerics.ShowMessage(response.Message);
+                                    BtnUpdate.Text = "Editar";
+                                    Tables_Load(sender, e);
+                                }
+                                else
+                                    ActionsGenerics.ShowMessage(response.Message);
+                            }
+                        }
+                        else
+                            ActionsGenerics.ShowMessage(response.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Write($"Erro ao tentar atualizar/salvar dados de uma mesa. Exception: {ex.Message} StackTrace: {ex.StackTrace}");
+            }
         }
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (ActionsGenerics.ConfirmCustom("Você deseja adicionar uma nova mesa?"))
+                {
+                    //Processo de adicionar no banco.
+                    _client = new HttpClient();
 
+                    var table = new Table
+                    {
+                        TableStatus = TableConstants.OPEN_VALUE,
+                        IsActive = true,
+                        CreationDate = DateTime.Now,
+                        CreationUserId = UserLogged.UserId
+                    };
+
+                    var request = JsonConvert.SerializeObject(table);
+                    var contentString = new StringContent(request, Encoding.UTF8, "application/json");
+                    contentString.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    HttpResponseMessage result = _client.PostAsync($"{_baseUrl}/Table", contentString).Result;
+                    var response = JsonConvert.DeserializeObject<MessageResponse<Product>>(result.Content.ReadAsStringAsync().Result);
+                    if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        //Após salvar o registro zerar todos os campos e atualizar a lista
+                        BtnUpdate.Text = "Editar";
+                        TxtTableNumber.Text = string.Empty;
+                        ChkTableStatus.Text = string.Empty;
+                        BtnAdd.Text = "Novo";
+
+                        //Carregar a lista.
+                        Tables_Load(sender, e);
+                        ActionsGenerics.ShowMessage(response.Message);
+                    }
+                    else
+                        ActionsGenerics.ShowMessage(response.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Write($"Erro ao adicionar uma nova mesa.Exception: {ex.Message} StackTrace: {ex.StackTrace}");
+            }
         }
 
         private void BtnRemove_Click(object sender, EventArgs e)
         {
+            try
+            {
+                BtnUpdate.Text = "Editar";
 
+                var listOfAllSelected = ListTables.SelectedItems.Cast<ListViewItem>().ToList();
+                if (listOfAllSelected?.Count > 0)
+                {
+                    if (ActionsGenerics.ConfirmCustom("Tem certeza que deseja excluir a mesa?"))
+                    {
+
+                        foreach (var i in listOfAllSelected)
+                        {
+                            //Id da mesa
+                            LblIdTable.Text = i.Text;
+                        }
+
+                        _client = new HttpClient();
+                        HttpResponseMessage result = _client.DeleteAsync($"{_baseUrl}/Table/Delete/{Convert.ToInt32(LblIdTable.Text)}").Result;
+                        var response = JsonConvert.DeserializeObject<MessageResponse<Table>>(result.Content.ReadAsStringAsync().Result);
+                        if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            ActionsGenerics.ShowMessage(response.Message);
+                            Tables_Load(sender, e);
+                        }
+                        else
+                            ActionsGenerics.ShowMessage(response.Message);
+                    }
+                }
+                else
+                {
+                    ActionsGenerics.ShowMessage("Selecione uma mesa para conseguir remover!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Write($"Erro ao remover uma mesa.Exception: {ex.Message} StackTrace: {ex.StackTrace}");
+            }
         }
     }
 }
